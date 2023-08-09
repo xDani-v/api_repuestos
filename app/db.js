e93b28502757c2a2ec14cbcc70080c54b0c9931e
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2023-08-09 10:40:12 -0500

    dotenv

--- app/db.js
+++ app/db.js
@@ -1,15 +1,19 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+// Carga las variables de entorno desde el archivo .env
+dotenv.config();
 
 // Configura los detalles de conexión a la base de datos PostgreSQL
 const pool = new Pool({
-    user: 'fl0user', // Reemplaza con tu nombre de usuario
-    host: 'ep-super-wave-00075584.ap-southeast-1.aws.neon.tech', // Reemplaza con la dirección del servidor PostgreSQL (puedes usar 'localhost' si está en la misma máquina)
-    database: 'dbrepuestos', // Reemplaza con el nombre de tu base de datos
-    password: 'LvBk61RwGUsI', // Reemplaza con tu contraseña (puedes dejarlo vacío si no hay contraseña)
-    port: 5432,// Puerto por defecto de PostgreSQL
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false, // Agrega esta línea para evitar problemas de certificados autofirmados (opcional)
-        sslmode: 'require', // Esta opción establece la conexión con SSL
+        rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true',
+        sslmode: process.env.DB_SSL_MODE,
     },
 });
 
